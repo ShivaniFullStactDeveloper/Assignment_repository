ee83f9d9b0a5bbb2679de2435ea268a6f453619e
ShivaniFullStactDeveloper <156500244+ShivaniFullStactDeveloper@users.noreply.github.com>
2026-02-03 09:59:00 +0530

    Delete src directory

--- src/routes/tenantRoute.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const router = express.Router();
-
-const tenantController = require('../controllers/tenantController');
-
-router.post('/tenant', tenantController.createTenant);
-router.get('/tenants', tenantController.getTenants);
-
-module.exports = router;
